cef6a2bec86e918a835188bdbae6b91a20c72bb5
Peyman Zarchini <peymanzarchini@outlook.com>
2023-03-13 19:53:23 +0330

    Complete the Oauth functionality

--- src/components/userAccount/OAuthButton.jsx
+++ src/components/userAccount/OAuthButton.jsx
@@ -1,11 +1,45 @@
 import { Button, useTheme } from "@mui/material";
 import GoogleIcon from "@mui/icons-material/Google";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { db } from "../../data/firebase";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 function OAuthButton() {
   const theme = useTheme();
+  const navigate = useNavigate();
+
+  async function handleGoogleAuth() {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      //check for the user
+
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      navigate("/");
+    } catch (error) {
+      toast.error("Could not authorize with Google");
+    }
+  }
+
   return (
     <>
       <Button
+        type="button"
+        onClick={handleGoogleAuth}
         variant="contained"
         fullWidth
         sx={{
